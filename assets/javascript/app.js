809ebbf6b5a86ce0ed66c777d641199d38bf98ed
Austin Parisi <austinparisi1991@gmail.com>
2017-08-29 16:36:55 -0500

    update

---
+++ assets/javascript/app.js
@@ -0,0 +1,60 @@
+$(document).ready(function() {
+
+	var timer = 30;
+
+	var correct = 0;
+
+	var wrong = 0;
+
+	var questions = [
+		"What birthday does Bilbo celebrate before leaving the Shire?", 
+		"Who do the Hobbits encounter at the Prancing Pony?",
+		"How many members make up the fellowship?",
+ 		"What's taters, Precious?",
+		"How does Sam suggest potatoes be cooked?",
+		"How many rings of power are there?",
+		"At the end of the trilogy, where does Frodo go?",
+		"Who is the captain of Gondor?",
+		"What group of wizards is Gandalf a part of?",
+		"What is the name of the Balrog that Gandalf fights in the Mines of Moria?"
+		];
+
+	var answers = [
+		["100th", "101st", "111th", "200th"],
+		["Gandalf", "Boromir", "Legolas", "Strider"],
+		["Nine", "Five", "Seven", "Ten"],
+		["PO-TA-TOES", "PO-TA-TOES", "PO-TA-TOES", "PO-TA-TOES"],
+		["Boil 'em", "Mash 'em", "Stick 'em in a stew", "All of the above"],
+		["5", "20", "8", "12"],
+		["Rivendell", "Bagend", "Gondor", "Undying Lands"],
+		["Boromir", "Faramir", "Aragorn", "Eowyn"],
+		["The Black Council", "The Istari", "Erigon", "The Shire"],
+		["Morgon", "Durin's Bane", "Morgoth", "Shadow and Flame"]
+		];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
